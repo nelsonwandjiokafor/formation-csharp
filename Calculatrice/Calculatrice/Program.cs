19f82ddd6bd3394ae2350ab24479689d8cb7cd5a
Nelson Wandji <wandjiokafor@yahoo.fr>
2026-09-03 15:52:01 +0200

    Projets Formation: Calculatrice und Einkaufsliste

---
+++ Calculatrice/Calculatrice/Program.cs
@@ -0,0 +1,56 @@
+﻿double LeseZahl(string nachricht)
+{
+    Console.Write(nachricht);
+    return Convert.ToDouble(Console.ReadLine());
+}
+
+double Berechnen(double zahl1, double zahl2, string operateur)
+{
+    if (operateur == "+")
+        return zahl1 + zahl2;
+    else if (operateur == "-")
+        return zahl1 - zahl2;
+    else if (operateur == "*")
+        return zahl1 * zahl2;
+    else if (operateur == "/")
+    {
+        if (zahl2 == 0)
+            return double.NaN;
+        return zahl1 / zahl2;
+    }
+    else
+        return double.NaN;
+}
+
+Console.WriteLine("Willkommen beim Taschenrechner");
+bool weiter = true;
+int anzahl = 0;
+
+while (weiter)
+{
+    double zahl1 = LeseZahl("Gib die erste Zahl ein: ");
+
+    Console.Write("Operator (+, -, *, /): ");
+    string operateur = Console.ReadLine();
+
+    double zahl2 = LeseZahl("Gib die zweite Zahl ein: ");
+
+    double resultat = Berechnen(zahl1, zahl2, operateur);
+
+    if (double.IsNaN(resultat))
+    {
+        Console.WriteLine("Fehler: Ungültige Berechnung");
+    }
+    else
+    {
+        Console.WriteLine($"Ergebnis: {resultat:F2}");
+        anzahl++;
+    }
+
+    Console.Write("Nochmal? (ja/nein): ");
+    string antwort = Console.ReadLine();
+    if (antwort == "nein")
+        weiter = false;
+}
+
+Console.WriteLine($"Du hast {anzahl} Berechnungen durchgeführt.");
---
+++ Einkaufsliste/Einkaufsliste/Program.cs
@@ -0,0 +1,81 @@
+﻿List<string> artikelListe = new List<string>();
+
+bool weiter = true;
+
+while (weiter)
+{
+    Console.WriteLine();
+    Console.WriteLine("=== EINKAUFSLISTE ===");
+    Console.WriteLine("1. Artikel hinzufügen");
+    Console.WriteLine("2. Artikel entfernen");
+    Console.WriteLine("3. Alle Artikel anzeigen");
+    Console.WriteLine("4. Artikel suchen");
+    Console.WriteLine("5. Beenden");
+    Console.Write("Auswahl: ");
+
+    string auswahl = Console.ReadLine();
+
+    switch (auswahl)
+    {
+        case "1":
+            Console.Write("Artikelname: ");
+            string neuerArtikel = Console.ReadLine();
+
+            artikelListe.Add(neuerArtikel);
+
+            Console.WriteLine($"{neuerArtikel} wurde hinzugefügt.");
+            break;
+
+
+        case "2":
+            Console.Write("Welchen Artikel möchtest du entfernen? ");
+            string artikelEntfernen = Console.ReadLine();
+
+            if (artikelListe.Contains(artikelEntfernen))
+            {
+                artikelListe.Remove(artikelEntfernen);
+                Console.WriteLine($"{artikelEntfernen} wurde entfernt.");
+            }
+            else
+            {
+                Console.WriteLine("Fehler: Artikel nicht gefunden.");
+            }
+
+            break;
+
+
+        case "3":
+            Console.WriteLine("=== DEINE ARTIKEL ===");
+
+            for (int i = 0; i < artikelListe.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}. {artikelListe[i]}");
+            }
+
+            break;
+
+        case "4":
+            Console.Write("Suchbegriff eingeben: ");
+            string suchbegriff = Console.ReadLine();
+
+            foreach (string artikel in artikelListe)
+            {
+                if (artikel.Contains(suchbegriff))
+                {
+                    Console.WriteLine($"Gefunden: {artikel}");
+                }
+            }
+
+            break;
+
+        case "5":
+            Console.WriteLine($"Programm beendet. Insgesamt {artikelListe.Count} Artikel in der Einkaufsliste.");
+            weiter = false;
+            break;
+
+
+        default:
+            Console.WriteLine("Ungültige Auswahl. Bitte 1 bis 5 eingeben.");
+            break;
+    }
+}
